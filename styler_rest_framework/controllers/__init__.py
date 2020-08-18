f61ad2569e883cb4cdc3266f506ff232226dfe7f
Bruno Toshio Sugano <brunotoshio@gmail.com>
2020-08-18 17:25:44 +0900

    controller logic

---
+++ styler_rest_framework/controllers/__init__.py
@@ -0,0 +1,135 @@
+""" Module for controllers
+"""
+
+import json
+import logging
+
+from aiohttp import web
+from styler_rest_framework.exceptions.services import \
+    InvalidDataError, AuthenticationError, AuthorizationError, UnexpectedError
+from styler_rest_framework.exceptions.business import \
+    ValidationError, NotFoundError, PermissionDeniedError
+from styler_identity import Identity
+
+
+def get_token_auth_header(request):
+    """ Obtains the Access Token from the Authorization Header
+    """
+    auth_header = request.headers.get('Authorization')
+    if not auth_header:
+        raise ValueError('Authorization header is expected')
+
+    parts = auth_header.split()
+
+    if parts[0].lower() != 'bearer':
+        raise ValueError('Authorization header must start with Bearer')
+    elif len(parts) == 1:
+        raise ValueError('Token not found')
+    elif len(parts) > 2:
+        raise ValueError('Authorization header must be Bearer token')
+
+    token = parts[1]
+    return token
+
+
+class BaseController:
+    """ Base class for controllers
+    """
+
+    def get_identity(self, request):
+        """ Get the identity of the logged user
+        """
+        try:
+            token = get_token_auth_header(request)
+            return Identity(token)
+        except ValueError:
+            self.bad_request({'jwt': 'invalid'})
+
+    def resp_for_creation(self, new_id):
+        """ Default response for creation methods
+        """
+        return web.json_response({'id': new_id}, status=201)
+
+    def response_ok(self, data):
+        """ Default response for 200 OK
+        """
+        return web.json_response(data)
+
+    def resp_for_listing(self, paginator, items):
+        """ Default response for listings with pagination
+        """
+        return web.json_response({
+            'items': items,
+            'pagination': paginator.get_info()
+        })
+
+    def payment_required(self, code, reason):
+        """ Default response for Error code 402
+        """
+        error = {
+            'code': code,
+            'reason': reason
+        }
+        logging.error('Payment required error: %s', error)
+        raise web.HTTPPaymentRequired(
+            text=json.dumps(error),
+            content_type='application/json')
+
+    def bad_request(self, errors, code='validation_error'):
+        """ Default response for Error code 400
+        """
+        error = {
+            'code': code,
+            'reason': errors
+        }
+        logging.error('Bad request error: %s', error)
+        raise web.HTTPBadRequest(
+            text=json.dumps(error),
+            content_type='application/json')
+
+    def not_found(self, msg=''):
+        """ Default response for Error code 404
+        """
+        logging.error('Not found error error: %s', msg)
+        raise web.HTTPNotFound(
+            content_type='application/json')
+
+    def unauthorized(self, msg=''):
+        """ Default response for Error code 401
+        """
+        logging.error('Unauthorized error: %s', msg)
+        raise web.HTTPUnauthorized(
+            content_type='application/json')
+
+    def forbidden(self, msg=''):
+        """ Default response for Error code 403
+        """
+        logging.error('Forbidden error: %s', msg)
+        raise web.HTTPForbidden(
+            content_type='application/json')
+
+    def handle_service_errors(self, ex):
+        """ Default method for handling service related errors
+        """
+        if isinstance(ex, InvalidDataError):
+            self.bad_request(ex.json_body.get('reason'))
+        elif isinstance(ex, AuthenticationError):
+            self.unauthorized()
+        elif isinstance(ex, AuthorizationError):
+            self.forbidden()
+        elif isinstance(ex, UnexpectedError):
+            raise web.HTTPInternalServerError()
+        else:
+            raise ex
+
+    def handle_business_errors(self, ex):
+        """ Default method for handling business related errors
+        """
+        if isinstance(ex, ValidationError):
+            self.bad_request(ex.errors)
+        elif isinstance(ex, PermissionDeniedError):
+            self.forbidden()
+        elif isinstance(ex, NotFoundError):
+            self.not_found()
+        else:
+            raise ex
