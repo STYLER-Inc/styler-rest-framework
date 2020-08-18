42a787d10803cab74b6c77a85749eebc9c9fcc5b
Bruno Toshio Sugano <brunotoshio@gmail.com>
2020-08-18 17:22:41 +0900

    business layer

---
+++ styler_rest_framework/business/__init__.py
@@ -0,0 +1,25 @@
+""" Module for business classes
+"""
+
+from functools import wraps
+
+from styler_rest_framework.datasource import session_scope
+
+
+def session_aware(func):
+    """ Decorator to wrap the method inside a session aware context.
+
+        It also injects the db_session in the function, necessary to
+        control flush, commit, and rollbacks.
+    """
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        with session_scope.create(args[0].db_engine) as db_session:
+            kwargs['db_session'] = db_session
+            return func(*args, **kwargs)
+    return wrapper
+
+
+class BaseBusiness:
+    """ Base class for business classes
+    """
