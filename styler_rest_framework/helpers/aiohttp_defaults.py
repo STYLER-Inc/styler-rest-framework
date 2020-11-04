2afcd2ed537c2be0deac83817c2fe050ebee23d6
Bruno Toshio Sugano <brunotoshio@gmail.com>
2020-11-04 17:35:08 +0900

    changes in defaults

--- styler_rest_framework/helpers/aiohttp_defaults.py
+++ styler_rest_framework/helpers/aiohttp_defaults.py
@@ -6,32 +6,37 @@ import logging
 from styler_middleware import handle_exceptions, handle_invalid_json
 from styler_rest_framework.logging import setup_logging
 
-reporting_handler = None
 
-try:  # pragma: no coverage
-    from google.cloud import error_reporting
-    client = error_reporting.Client()
-    reporting_handler = client.report_exception
-except Exception:
-    logging.warning(
-        '''
-        Could not find error reporting,
-        please run pip install google-cloud-error-reporting
-        '''
-    )
+def google_error_reporting_handler():  # pragma: no coverage
+    try:
+        from google.cloud import error_reporting
+        client = error_reporting.Client()
+        def error_handler(request, exc):
+            client.report_exception
+        return error_handler
+    except Exception:
+        logging.warning(
+            '''
+            Could not find error reporting,
+            please run pip install google-cloud-error-reporting
+            '''
+        )
+        return None
 
 
-def add_middlewares(app):
+def add_middlewares(app, error_handler=None):
     """ Append default middlewares
     """
+    if not error_handler:
+        error_handler = google_error_reporting_handler()
     app.middlewares.extend([
-        handle_exceptions(error_handler=reporting_handler),
+        handle_exceptions(error_handler=error_handler),
         handle_invalid_json()
     ])
 
 
-def set_logging():  # pragma: no coverage
-    setup_logging(logging.INFO)
+def set_logging(level=logging.INFO):  # pragma: no coverage
+    setup_logging(level)
 
 
 def default_configuration(app):  # pragma: no coverage
