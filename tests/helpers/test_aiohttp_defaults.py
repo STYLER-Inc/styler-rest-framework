d5e8477f4795ff4930c827b9cb94537fc0cbcd58
Bruno Toshio Sugano <brunotoshio@gmail.com>
2020-11-04 17:35:18 +0900

    tests

--- tests/helpers/test_aiohttp_defaults.py
+++ tests/helpers/test_aiohttp_defaults.py
@@ -16,3 +16,14 @@ class TestAddMiddlewares:
         assert len(app.middlewares) == 2
         assert callable(app.middlewares[0])
         assert callable(app.middlewares[1])
+
+    def test_add_middlewares_with_custom_error_handler(self):
+        app = Mock()
+        app.middlewares = []
+        error_handler = Mock()
+
+        aiohttp_defaults.add_middlewares(app, error_handler=error_handler)
+
+        assert len(app.middlewares) == 2
+        assert callable(app.middlewares[0])
+        assert callable(app.middlewares[1])
