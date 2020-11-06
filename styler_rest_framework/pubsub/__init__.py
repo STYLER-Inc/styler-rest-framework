4e6c7e8803267071bdee13af42635a9fe61c869e
Bruno Toshio Sugano <brunotoshio@gmail.com>
2020-11-06 13:20:35 +0900

    changes

--- styler_rest_framework/pubsub/__init__.py
+++ styler_rest_framework/pubsub/__init__.py
@@ -1,37 +1,66 @@
-""" pubsub module
+""" Message handling module
 """
 
-from typing import Any, Callable
+from typing import Any, Callable, Dict
+import json
 import logging
 
-from google.cloud import pubsub_v1
+from styler_rest_framework.logging import error_reporting
 
 
-def configure_subscriber(handler: Callable[[Any], None], sub_name: str) -> Any:
-    """ Configure a subscriber
-
-        Args:
-            handler: message handling callback
-            sub_name: Subscription name
-
-        Returns:
-            A subscriber listener
-    """ subscriber = pubsub_v1.SubscriberClient()
-    return subscriber.subscribe(
-        sub_name, callback=handler
-    )
-
-
-def start_listening(sub: Any) -> None:
-    """ Start listening for messages
-
-        Args:
-            sub: Subscriber listener
+class MessageRouter:
+    """ Handles and routes messages to mapping handlers
     """
-    logging.info('Start listening...')
-    try:
-        sub.result()
-    except Exception as exc:
-        logging.exception('Stopped listening: %s', exc)
-        sub.cancel()
 
+    def __init__(self, error_handler=None):
+        self._routes = {}
+        self.error_handler = error_handler or \
+            error_reporting.google_error_reporting_handler()
+
+    def handle_message(self, message: Any) -> None:
+        """ Route the incoming messages
+
+            Args:
+                message: message received from pull
+        """
+        logging.info('Received message: %s', message)
+
+        message.ack()
+
+        try:
+            body = self._validate_envelope(message.data)
+
+            message_name = body['name']
+            if message_name in self._routes:
+                self._routes[message_name](body['arg'])
+            else:
+                logging.info('Unknown message name: %s', message_name)
+        except Exception:
+            if self.error_handler:
+                self.error_handler()
+            logging.exception('Error when handling the message')
+
+    def add_route(self,
+                  message_name: str,
+                  message_handler: Callable[[Any, Any], None]) -> None:
+        """ Register a message route
+
+            Args:
+                message_name: expected message name.
+                message_handler: function that receives a database connection
+                    and the args received in the message.
+        """
+        self._routes[message_name] = message_handler
+
+    def _validate_envelope(self, data: str) -> (bool, Dict):
+        """ Validates the envelope of the data
+
+            Args:
+                data: the data received from the message
+            Returns:
+                The JSON parsed body data
+        """
+        body = json.loads(data)
+        if 'name' not in body or 'arg' not in body:
+            raise KeyError('Missing name or arg in the message body')
+        return body
