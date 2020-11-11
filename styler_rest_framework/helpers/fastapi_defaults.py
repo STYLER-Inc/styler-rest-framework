e4ca87bb602dec6298357f2aa1da7a1070d55519
Bruno Toshio Sugano <brunotoshio@gmail.com>
2020-11-11 16:57:37 +0900

    adding exception middleware

---
+++ styler_rest_framework/helpers/fastapi_defaults.py
@@ -0,0 +1,49 @@
+""" Define helper methods to reduce the boilerplate code for a server
+"""
+
+import logging
+
+from styler_rest_framework.config import defaults
+from styler_rest_framework.middlewares.fastapi import exception_middleware
+from styler_rest_framework.logging.error_reporting import (
+    google_error_reporting_handler
+)
+
+
+def api_error_reporting_handler(service=None):  # pragma: no coverage
+    try:
+        service_name = service or defaults.ERROR_HANDLER_SERVICE
+        from google.cloud import error_reporting
+        handler = google_error_reporting_handler(service=service_name)
+
+        def error_handler(request, exc):
+            http_context = error_reporting.HTTPContext(
+                method=request.method, url=request.path)
+            handler(http_context=http_context)
+
+        return error_handler
+    except Exception:
+        logging.warning('Could not find start api error reporting')
+        return None
+
+
+def add_middlewares(
+            app,
+            error_handler=None,
+            service=None,
+            handle_exceptions_args=None
+        ):  # pragma: no coverage
+    """ Append default exception middleware
+
+        Args:
+            app: FastAPI app
+            error_handler: exception handler
+            service: service name
+            handle_exceptions_args: dict of parameters to the middleware
+    """
+    handle_exceptions_args = handle_exceptions_args or {}
+    error_handler = error_handler or api_error_reporting_handler(
+        service=service)
+
+    exception_middleware.add_exception_middleware(
+        app, error_handler=error_handler, **handle_exceptions_args)
---
+++ styler_rest_framework/middlewares/fastapi/exception_middleware.py
@@ -0,0 +1,40 @@
+""" Middleware to handle exceptions
+"""
+import logging
+
+from fastapi import FastAPI, Request, HTTPException
+from fastapi.responses import JSONResponse
+
+
+def add_exception_middleware(
+        app: FastAPI,
+        generic_message='An error has occurred',
+        status_code=500,
+        error_handler=None):
+    """ Generate a middleware that logs unexpected exceptions
+        and returns a JSON response.
+        Exceptions of type HTTPException won't be handled as they should be
+        expected exceptions.
+
+        Args:
+            app: FastAPI application
+            generic_message: The message that will be send as an error
+            status_code: The HTTP status code (default = 500)
+            error_handler: Callable(request, exception)
+    """
+    @app.middleware('http')
+    async def handle_exception(request: Request, call_next):
+        try:
+            response = await call_next(request)
+            return response
+        except HTTPException:
+            raise
+        except Exception as ex:
+            message = str(ex)
+            if error_handler:
+                error_handler(request, ex)
+            logging.exception('Error: %s', message)
+            return JSONResponse(
+                status_code=status_code,
+                content={'error': generic_message},
+            )
