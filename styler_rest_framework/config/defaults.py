ac7141114066e5e4c7a24b29d5bff7bf0c585445
Bruno Toshio Sugano <brunotoshio@gmail.com>
2020-11-12 19:13:53 +0900

    adding velidation error handler

--- styler_rest_framework/config/defaults.py
+++ styler_rest_framework/config/defaults.py
@@ -6,5 +6,6 @@ import os
 
 SERVICE_NAME = os.getenv('SERVICE_NAME') or 'svc'
 NAMESPACE = os.getenv('NAMESPACE') or 'namespace'
+VERSION = os.getenv('VERSION') or '0.1.0'
 
 ERROR_HANDLER_SERVICE = f'{NAMESPACE}/{SERVICE_NAME}'
