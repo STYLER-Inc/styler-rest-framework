3c19744998c1f2c5c27df7cd9c24167ba6a6d8c3
Bruno Toshio Sugano <brunotoshio@gmail.com>
2021-01-05 14:41:46 +0900

    adding identity

--- styler_rest_framework/api/request_scope.py
+++ styler_rest_framework/api/request_scope.py
@@ -3,11 +3,80 @@
 
 from typing import Dict, Callable
 
-from styler_identity import Identity
+from jwt.exceptions import InvalidTokenError
+import jwt
+
+
+class Identity:
+    """Holds the identity of the logged user
+    """
+    def __init__(self, token):
+        self._token = token
+        try:
+            self._decoded = jwt.decode(
+                self._token, options={"verify_signature": False})
+        except InvalidTokenError:
+            raise ValueError('Invalid JWT token')
+
+    def user_id(self):
+        """Returns the user_id provided by firebase
+        """
+        return self._decoded.get('user_id')
+
+    def is_system_admin(self):
+        """Returns a boolean identifying the user as a system administrator
+        """
+        return 'sysadmin' in self._roles()
+
+    def is_admin(self):
+        """Returns a boolean identifying the user
+        as an organization administrator
+        """
+        return 'admin' in self._roles()
+
+    def is_staff(self):
+        """Returns a boolean identifying the user
+        as a shop staff
+        """
+        return 'staff' in self._roles()
+
+    def shops(self):
+        """Returns a list of shop_ids that the user has access to
+        """
+        return self._custom_claims().get('shop', [])
+
+    def organizations(self):
+        """Returns a list of organization_ids that the user has access to
+        """
+        return self._custom_claims().get('organization', [])
+
+    def data(self):
+        """Return the entire data from the token
+        """
+        return self._decoded
+
+    def token(self):
+        """Returns the original JWT token
+        """
+        return self._token
+
+    def _roles(self):
+        """Returns the collection of roles
+        """
+        if 'roles' not in self._decoded:
+            return []
+        return self._decoded['roles']
+
+    def _custom_claims(self):
+        """Returns the collection of custom claims
+        """
+        if 'claims' not in self._decoded:
+            return {}
+        return self._decoded['claims']
 
 
 class RequestScope(Identity):
-    """ Carries information about the request scope
+    """Carries information about the request scope
     """
     def __init__(
         self,
@@ -40,7 +109,7 @@ class RequestScope(Identity):
 
 
 def get_token(authorization: str) -> str:
-    """ Obtains the Access Token from the Authorization Header value
+    """Obtains the Access Token from the Authorization Header value
     """
     if not authorization:
         raise ValueError('Authorization header is expected')
