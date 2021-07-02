80473a130a80966c601ae0ba5fe78e61773476ca
Bruno Toshio Sugano <brunotoshio@gmail.com>
2021-07-02 17:51:18 +0900

    Bump version: 1.8.1 → 1.9.0

--- setup.py
+++ setup.py
@@ -75,6 +75,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/STYLER-Inc/styler-rest-framework',
-    version='1.8.1',
+    version='1.9.0',
     zip_safe=False,
 )
