c43c3d217a63830d8e192a18b814b5ab6320b7be
Bruno Toshio Sugano <brunotoshio@gmail.com>
2022-02-15 17:06:41 +0900

    Bump version: 1.14.0 → 1.14.1

--- setup.py
+++ setup.py
@@ -77,6 +77,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/STYLER-Inc/styler-rest-framework',
-    version='1.14.0',
+    version='1.14.1',
     zip_safe=False,
 )
